260bdb11111bcd47b9102ccd714b6d16a68e4bfc
jimmy <jimmy0xyz@gmail.com>
2025-02-24 16:21:20 +0800

    update version number to 0.1.1

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 const (
 	// CoreSemVer represents the core semantic version when not using git describe.
 	// It follows the semantic versioning format.
-	CoreSemVer = "0.1.0"
+	CoreSemVer = "0.1.1"
 )
 
 // GitCommitHash uses git rev-parse HEAD to find commit hash which is helpful
