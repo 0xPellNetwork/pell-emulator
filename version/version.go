18d3fb3c5d1d929c1b72ea2f3f836d7e7e60451f
Jimmy <jimmy@jimmyzai.com>
2025-03-03 11:27:02 +0800

    bump new version: v0.1.2 (#3)
    Signed-off-by: jimmy <jimmy0xyz@gmail.com>

--- version/version.go
+++ version/version.go
@@ -3,7 +3,7 @@ package version
 const (
 	// CoreSemVer represents the core semantic version when not using git describe.
 	// It follows the semantic versioning format.
-	CoreSemVer = "0.1.1"
+	CoreSemVer = "0.1.2"
 )
 
 // GitCommitHash uses git rev-parse HEAD to find commit hash which is helpful
